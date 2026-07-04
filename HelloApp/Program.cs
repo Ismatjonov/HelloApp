dd0c74c000cbd67097464b0c9e40005e7e5beef1
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-04 17:11:34 +0500

    Setting headers

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,13 +1,21 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// int x = 2;
+// app.Run(async (context) =>
+// {
+//     x = x * 2;
+//     await context.Response.WriteAsync("Result: " + x);
+// });
 
-app.Run(HandleRequest);
-app.Run();
-
-// writing middleware in separated method
-async Task HandleRequest(HttpContext context)
+// Setting Headers
+app.Run(async (context) =>
 {
-    await context.Response.WriteAsync("Hello Bakhtovar!!");
-}
+    var response = context.Response;
+    response.Headers.ContentLanguage = "ru-RU";
+    response.Headers.ContentType = "text/plain; charset=utf-8";
+    response.Headers.Append("secret-id", "256");
+    await response.WriteAsync("Привет мир!");
+});
+
+app.Run();

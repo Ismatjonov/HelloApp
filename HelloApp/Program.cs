af5a70caba1c5742b8ac62edb0a670bfee943bb0
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-07 17:23:47 +0500

    complex tasks

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -106,10 +106,29 @@ WebApplication app = builder.Build();
 });*/
 
 // ---- sending html-files ----
-app.Run(async context =>
+/*app.Run(async context =>
 {
     context.Response.ContentType = "text/html charset=utf-8";
     await context.Response.SendFileAsync("html/index.html");
+});*/
+
+// ---- complex tasks ----
+app.Run(async context =>
+{
+    var path = context.Request.Path;
+    var fullPath = $"html/{path}";
+    var response = context.Response;
+    
+    response.ContentType = "text/html charset=utf-8";
+    if (File.Exists(fullPath))
+    {
+        response.SendFileAsync(fullPath);
+    }
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsync("<h2>Not Found!</h2>");
+    }
 });
 
 app.Run();

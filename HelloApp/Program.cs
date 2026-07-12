37fb6d74a96e2eee6515cddeeaf6245538049072
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-12 22:04:16 +0500

    Creating simple APIs

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.FileProviders;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -253,7 +254,7 @@ WebApplication app = builder.Build();
 });*/
 
 // ----- Setting serialization -----
-app.Run(async context =>
+/*app.Run(async context =>
 {
     var response = context.Response;
     var request = context.Request;
@@ -268,9 +269,9 @@ app.Run(async context =>
 
         if (request.HasJsonContentType())
         {
-            var jsonOption = new JsonSerializerOptions();
-            jsonOption.Converters.Add(new PersonConverter());
-            var person = await request.ReadFromJsonAsync<Person>(jsonOption);
+            var jsonOptions = new JsonSerializerOptions();
+            jsonOptions.Converters.Add(new PersonConverter());
+            var person = await request.ReadFromJsonAsync<Person>(jsonOptions);
             if (person != null)
                 responseText = $"Name: {person.Name}, Age: {person.Age}";
         }
@@ -282,13 +283,169 @@ app.Run(async context =>
         response.ContentType = "text/html; charset=utf-8";
         await response.SendFileAsync("html/index.html");
     }
+});*/
+
+
+// =============== CREATING AN SIMPLE API ===============
+List<Person> users = new List<Person> 
+{ 
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+ 
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
+    //string expressionForNumber = "^/api/users/([0-9]+)$";   // если id представляет число
+ 
+    // 2e752824-1657-4c7f-844b-6ec2e168e99c
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+    if (path == "/api/users" && request.Method=="GET")
+    {
+        await GetAllPeople(response); 
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
+    {
+        // получаем id из адреса url
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response);
+    }
+    else if (path == "/api/users" && request.Method == "POST")
+    {
+        await CreatePerson(response, request);
+    }
+    else if (path == "/api/users" && request.Method == "PUT")
+    {
+        await UpdatePerson(response, request);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "DELETE")
+    {
+        string? id = path.Value?.Split("/")[3];
+        await DeletePerson(id, response);
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
 });
-
+ 
 app.Run();
+ 
+// получение всех пользователей
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(users);
+}
+// получение одного пользователя по id
+async Task GetPerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, отправляем его
+    if (user != null)
+        await response.WriteAsJsonAsync(user);
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
+ 
+async Task DeletePerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, удаляем его
+    if (user != null)
+    {
+        users.Remove(user);
+        await response.WriteAsJsonAsync(user);
+    }
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
+ 
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        var user = await request.ReadFromJsonAsync<Person>();
+        if (user != null)
+        {
+            // устанавливаем id для нового пользователя
+            user.Id = Guid.NewGuid().ToString();
+            // добавляем пользователя в список
+            users.Add(user);
+            await response.WriteAsJsonAsync(user);
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
+ 
+async Task UpdatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        Person? userData = await request.ReadFromJsonAsync<Person>();
+        if (userData != null)
+        {
+            // получаем пользователя по id
+            var user = users.FirstOrDefault(u => u.Id == userData.Id);
+            // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+            if (user != null)
+            {
+                user.Age = userData.Age;
+                user.Name = userData.Name;
+                await response.WriteAsJsonAsync(user);
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            }
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
 
-public record class Person(string Name, int Age);
+// = = = = = = CLASSES = = = = = = 
 
-public class PersonConverter : JsonConverter<Person>
+public class Person
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+/*
+ public class PersonConverter : JsonConverter<Person>
 {
     public override Person Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
@@ -329,7 +486,7 @@ public class PersonConverter : JsonConverter<Person>
         writer.WriteStartObject();
         writer.WriteString("name", person.Name);
         writer.WriteNumber("age", person.Age);
-        
+
         writer.WriteEndObject();
     }
-}
+}*/
